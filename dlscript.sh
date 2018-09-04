766b074d863fce3aa40eef05b865969aa3bb57f3
VermontHen <42880269+VermontHen@users.noreply.github.com>
2018-09-03 23:27:50 -0400

    Update dlscript.sh

--- dlscript.sh
+++ dlscript.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
 
-#Ascertain name of txt file where the Youtube links are stored
-
-read -p "Enter file name here: " FILENAME
-
-#Dowload Youtube videos from links, this requires the youtube-dl program
-
-for LINKS in (cat $FILENAME); do
+for LINKS in $(cat links.txt); do
 youtube-dl $LINKS
 done
 
-#Let user know the program is finished
-
-echo "Videos finished downloading."
+echo "Done Downloading"
