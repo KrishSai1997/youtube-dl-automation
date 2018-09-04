9801eb3d93ac5d2f21acf8db7f4353a88470deee
William Seabolt <williamseabolt@Billys-MacBook-Pro.local>
2018-09-04 12:53:38 -0400

    Unwanted feedback suppression fixed.
    Suppresses unwanted feedback from the youtube-dl command

---
+++ dlscript2.1.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+#Ascertain which file to read youtube links from
+
+read -p "Enter file name here: " FILENAME
+
+#Begin automated download process
+
+if [ -e $FILENAME ]; then
+
+for LINKS in (cat $FILENAME); do
+
+{
+  youtube-dl $LINKS
+} &> /dev/null
+
+#Provide feedback on success or failure of each download
+
+if [ $? = 0 ]; then
+  echo "Video Downloaded"
+else
+  echo "Download Failed"
+fi
+
+done
+
+#Provide feedback if improper file name given
+
+else
+  echo "File does not exist, please run script again."
+fi
+
+#Provide feedback at completion of script
+
+echo "Script complete."
